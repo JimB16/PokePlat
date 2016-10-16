40729ed8bb8236d8e3ba11c56f7153c373a609ef
JimB16 <f1@jimb.de>
2016-10-16 11:14:43 +0200

    updated one map-script

--- data/fielddata/script/scr_seq/data_00000413.s
+++ data/fielddata/script/scr_seq/data_00000413.s
@@ -30,10 +30,8 @@ Script_1: @ 12
 	If 0x8008, 0x5
 	CompareLastResultJump EQUAL, Script_branch_21b
 	Jump Script_branch_33e
-@ 80
-
-.byte 0x2 @ 0x80
-.byte 0x0 @ 0x81
+	End
+@ 82
 
 Script_branch_82: @ 82
 	Call Function_158
@@ -43,16 +41,12 @@ Script_branch_82: @ 82
 	Cmd_346 0x0
 	Message 0xd
 	Jump Script_branch_1f2
-@ a6
-
 Script_branch_a6: @ a6
 	Cmd_33c 0x0, 0x8000
 	Cmd_346 0x0
 	Message 0x1f
 	WaitButton
 	Jump Script_branch_53a
-@ b9
-
 Script_branch_b9: @ b9
 	Call Function_158
 	If 0x800c, 0x0
@@ -60,15 +54,11 @@ Script_branch_b9: @ b9
 	Cmd_17b 0x0, 0x8000, 0x8001
 	Message 0xe
 	Jump Script_branch_1f2
-@ dc
-
 Script_branch_dc: @ dc
 	Cmd_17b 0x0, 0x8000, 0x8001
 	Message 0x20
 	WaitButton
 	Jump Script_branch_53a
-@ ee
-
 Script_branch_ee: @ ee
 	Call Function_158
 	If 0x800c, 0x0
@@ -76,15 +66,11 @@ Script_branch_ee: @ ee
 	Cmd_17b 0x0, 0x8000, 0x8001
 	Message 0xf
 	Jump Script_branch_1f2
-@ 111
-
 Script_branch_111: @ 111
 	Cmd_17b 0x0, 0x8000, 0x8001
 	Message 0x21
 	WaitButton
 	Jump Script_branch_53a
-@ 123
-
 Script_branch_123: @ 123
 	Call Function_158
 	If 0x800c, 0x0
@@ -92,15 +78,11 @@ Script_branch_123: @ 123
 	Cmd_17b 0x0, 0x8000, 0x8001
 	Message 0x10
 	Jump Script_branch_1f2
-@ 146
-
 Script_branch_146: @ 146
 	Cmd_17b 0x0, 0x8000, 0x8001
 	Message 0x22
 	WaitButton
 	Jump Script_branch_53a
-@ 158
-
 Function_158: @ 158
 	CheckFacePosition 0x800c
 	If 0x800c, 0x0
@@ -150,8 +132,6 @@ Script_branch_1f2: @ 1f2
 	WaitButton
 	Cmd_184 0x1
 	Jump Script_branch_53a
-@ 21b
-
 Script_branch_21b: @ 21b
 	SetVariableNumber 0x1, 0x8001
 	If 0x8001, 0x1
@@ -159,8 +139,6 @@ Script_branch_21b: @ 21b
 	Cmd_33c 0x0, 0x8000
 	Message 0x23
 	Jump Script_branch_243
-@ 23b
-
 Script_branch_23b: @ 23b
 	Cmd_33d 0x0, 0x8000
 	Message 0x15
@@ -177,8 +155,6 @@ Script_branch_243: @ 243
 	SetVarItem 0x0, 0x8000
 	Message 0x25
 	Jump Script_branch_291
-@ 289
-
 Script_branch_289: @ 289
 	Cmd_33d 0x0, 0x8000
 	Message 0x19
@@ -191,8 +167,6 @@ Script_branch_291: @ 291
 	CompareLastResultJump GREATER, Script_branch_2b4
 	SetVarItem 0x1, 0x8000
 	Jump Script_branch_2b9
-@ 2b4
-
 Script_branch_2b4: @ 2b4
 	Cmd_33d 0x1, 0x8000
 Script_branch_2b9: @ 2b9
@@ -201,30 +175,22 @@ Script_branch_2b9: @ 2b9
 	Message 0x1b
 	WaitButton
 	Jump Script_branch_53a
-@ 2cf
-
 Script_branch_2cf: @ 2cf
 	If 0x8001, 0x1
 	CompareLastResultJump GREATER, Script_branch_2e7
 	SetVarItem 0x0, 0x8000
 	Jump Script_branch_2ec
-@ 2e7
-
 Script_branch_2e7: @ 2e7
 	Cmd_33d 0x0, 0x8000
 Script_branch_2ec: @ 2ec
 	Message 0x1a
 	WaitButton
 	Jump Script_branch_53a
-@ 2f7
-
 Script_branch_2f7: @ 2f7
 	Call Function_158
 	If 0x800c, 0x0
 	CompareLastResultJump EQUAL, Script_branch_310
 	Jump Script_branch_1f2
-@ 310
-
 Script_branch_310: @ 310
 	SetVarHero 0x0
 	If 0x8001, 0x1
@@ -232,16 +198,12 @@ Script_branch_310: @ 310
 	SetVarItem 0x1, 0x8000
 	Message 0x24
 	Jump Script_branch_336
-@ 32e
-
 Script_branch_32e: @ 32e
 	Cmd_33d 0x1, 0x8000
 	Message 0x18
 Script_branch_336: @ 336
 	WaitButton
 	Jump Script_branch_53a
-@ 33e
-
 Script_branch_33e: @ 33e
 	Cmd_17f 0x800c
 	If 0x800c, 0x0
@@ -253,8 +215,6 @@ Script_branch_33e: @ 33e
 	SetVarItem 0x0, 0x800c
 	Message 0x1
 	Jump Script_branch_4da
-@ 374
-
 Script_branch_374: @ 374
 	CheckFacePosition 0x800c
 	If 0x800c, 0x0
@@ -267,8 +227,6 @@ Script_branch_374: @ 374
 	CompareLastResultJump EQUAL, Script_branch_540
 	Message 0x0
 	Jump Script_branch_4da
-@ 3b4
-
 Script_branch_3b4: @ 3b4
 	Message 0x0
 	Cmd_17a 0x4, 0x800c
@@ -287,16 +245,12 @@ Script_branch_3b4: @ 3b4
 	If 0x8008, 0x2
 	CompareLastResultJump EQUAL, Script_branch_53a
 	Jump Script_branch_53a
-@ 413
-
 Script_branch_413: @ 413
 	Message 0x5
 	YesNoBox 0x800c
 	If 0x800c, 0x0
 	CompareLastResultJump EQUAL, Script_branch_42d
 	Jump Script_branch_53a
-@ 42d
-
 Script_branch_42d: @ 42d
 	CloseMsgOnKeyPress
 	FadeScreen 0x6, 0x1, 0x0, 0x0
@@ -318,8 +272,6 @@ Script_branch_42d: @ 42d
 	If 0x8008, 0x62
 	CompareLastResultJump EQUAL, Script_branch_49d
 	Jump Script_branch_53c
-@ 49d
-
 Script_branch_49d: @ 49d
 	GiveItem 0x8005, 0x1, 0x8004
 	CheckDeoxis 0x8005
@@ -329,15 +281,11 @@ Script_branch_49d: @ 49d
 	SetVarItem 0x0, 0x8005
 	Message 0xb
 	Jump Script_branch_4da
-@ 4ca
-
 Script_branch_4ca: @ 4ca
 	SetVarItem 0x0, 0x8005
 	Message 0x1e
 	WaitButton
 	Jump Script_branch_53a
-@ 4da
-
 Script_branch_4da: @ 4da
 	Message 0x8
 	YesNoBox 0x800c
@@ -360,8 +308,6 @@ Script_branch_4ee: @ 4ee
 	GiveItem 0x800c, 0x1, 0x8004
 	Cmd_183 0x800c
 	Jump Script_branch_53a
-@ 53a
-
 Script_branch_53a: @ 53a
 	CloseMsgOnKeyPress
 Script_branch_53c: @ 53c
@@ -373,16 +319,12 @@ Script_branch_540: @ 540
 	Message 0x1c
 	WaitButton
 	Jump Script_branch_53a
-@ 54b
-
 Script_branch_54b: @ 54b
 	Cmd_17f 0x800c
 	SetVarItem 0x0, 0x800c
 	Message 0x1d
 	WaitButton
 	Jump Script_branch_53a
-@ 55f
-
 Script_2: @ 55f
 	PlayFanfare 0x5dc
 	LockAll
